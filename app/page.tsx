451862373c620358ffb4b3830e710062f11b9b56
Carmelo Atienza <atienzabusiness@gmail.com>
2024-05-05 00:00:45 +0800

    refactor: update navigation items

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "../components/custom/Navbar";
 export default function Home() {
   return (
     <main>
-      <section className = "p-12">
+      <section className = "p-12 bg-gradient-to-b from-green-200 to-green-50">
         <Navbar />
         <div className = "flex justify-center flex-col gap-10">
           <span className = "lg:text-9xl md:text-5xl sm: text-3xl text-green-600 font-medium text-center">
--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -3,9 +3,8 @@ import React from 'react';
 function Navbar(){
     const navigation = [
         { name: 'Home', href: '#' },
-        { name: 'About', href: '#' },
-        { name: 'Services', href: '#' },
-        { name: 'Contact', href: '#' },
+        { name: 'Dashboard', href: '#' },
+        { name: 'Docs', href: '#' },
     ]
     return (
       <nav className = "sticky top-0 bg-green-100/80 p-4 mb-12 rounded-full shadow-md border-2 border-green-300 px-12">
