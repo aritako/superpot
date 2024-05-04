46383fdca31866bd2e9a12f729e28ac1c6f3b465
Carmelo Atienza <atienzabusiness@gmail.com>
2024-05-05 01:53:02 +0800

    feat: add max-width for navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import Navbar from "../components/custom/Navbar";
 export default function Home() {
   return (
     <main>
-      <section className = "p-12 bg-gradient-to-b from-green-200 to-green-50">
-        <Navbar />
+      <section className = "lg:p-12 sm:p-6 p-6 bg-gradient-to-b from-green-200 to-green-50">
+        <div className = "flex justify-center">
+          <Navbar />
+        </div>
         <div className = "flex justify-center flex-col gap-10">
-          <span className = "lg:text-9xl md:text-5xl sm: text-3xl text-green-600 font-medium text-center">
+          <span className = "lg:text-9xl sm:text-7xl text-5xl text-green-600 font-medium text-center">
             SuperPot.
           </span>
           <span className = "text-center py">Coming Soon...</span>
