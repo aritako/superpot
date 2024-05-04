13d793c1806008bf231143dfeb10de873a693a82
Carmelo Atienza <atienzabusiness@gmail.com>
2024-05-04 22:53:30 +0800

    feat: setup navbar styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@ import Navbar from "../components/custom/Navbar";
 
 export default function Home() {
   return (
-    <>
-    {/* <Navbar /> */}
-    <main className="p-24 bg-green-50 h-100%">
-      <div className = "flex justify-center flex-col gap-10">
-        <span className = "md:text-7xl text-5xl text-green-600 font-medium text-center">
-          SuperPot.
-        </span>
-        <span className = "text-center">Coming Soon...</span>
-      </div>
+    <main>
+      <section className = "p-12">
+        <Navbar />
+        <div className = "flex justify-center flex-col gap-10">
+          <span className = "lg:text-9xl md:text-5xl sm: text-3xl text-green-600 font-medium text-center">
+            SuperPot.
+          </span>
+          <span className = "text-center py">Coming Soon...</span>
+        </div>
+      </section>
     </main>
-    </>
   );
 }
--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 function Navbar(){
+    const navigation = [
+        { name: 'Home', href: '#' },
+        { name: 'About', href: '#' },
+        { name: 'Services', href: '#' },
+        { name: 'Contact', href: '#' },
+    ]
     return (
-      <nav>
-        Hello World
+      <nav className = "sticky top-0 bg-green-100/80 p-4 mb-12 rounded-full shadow-md border-2 border-green-300 px-12">
+        SuperPot
       </nav>
     );
 };
