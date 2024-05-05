605ce6fa262dffa98634521c07e8841aae4a9213
Carmelo Atienza <atienzabusiness@gmail.com>
2024-05-05 16:28:43 +0800

    fix: adjust themings for SuperPot

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,6 +2,11 @@ export default {
     logo: <span>Super Pot</span>,
     project: {
       link: 'https://github.com/aritako/superpot'
+    },
+    docsRepositoryBase: "https://github.com/aritako/superpot/tree/main/pages",
+    useNextSeoProps(){
+      return{
+        titleTemplate: '%s | SuperPot',
+      }
     }
-    // ... other theme options
 }
