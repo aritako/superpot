e0365ffaa12c14d02cc71d8ca940c9edcba00ba7
Herminio Gregorio <hlgregorio@up.edu.ph>
2024-05-09 12:12:49 +0800

    feat: add login loading screen

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,17 +10,26 @@ export default function Dashboard() {
       redirect("/login");
     },
   });
-
-  return (
-    <section className="flex-grow lg:p-12 sm:p-6 p-6 bg-gradient-to-b from-green-200 to-green-50">
-      <div className="flex justify-center">
-        <Navbar />
-      </div>
-      <div className="flex justify-center flex-col gap-10">
-        <span className="lg:text-lg text-black-600 text-green-800 text-center mt-[-30px]">
-          🌱 Dashboard Page Under Construction...
-        </span>
-      </div>
-    </section>
-  );
+  if (!session) {
+    return (
+      <section className="flex-grow lg:p-12 sm:p-6 p-6 bg-gradient-to-b from-green-200 to-green-50">
+        <div className="flex justify-center">
+          <Navbar />
+        </div>
+        <div className="flex justify-center flex-col gap-10">
+          <span className="lg:text-lg text-black-600 text-green-800 text-center mt-[-30px]">
+            🌱 Dashboard Page Under Construction...
+          </span>
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <section className="flex-grow lg:p-12 sm:p-6 p-6 bg-gradient-to-b from-green-200 to-green-50">
+        <h1 className="flex text-xl items-center justify-center h-screen">
+          Loading... 🌱
+        </h1>
+      </section>
+    );
+  }
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,7 +8,11 @@ async function Login() {
     <section className="flex-grow lg:p-12 sm:p-6 p-6 bg-gradient-to-b from-green-200 to-green-50">
       <Navbar />
       <div className="flex flex-col items-center my-14">
-        <img src="/img/logo_green.png" alt="Logo" className="w-1/6" />
+        <img
+          src="/img/logo_green.png"
+          alt="Logo"
+          className="w-1/6 mt-[-30px]"
+        />
         <h1 className="text-2xl mb-[-35px]">Welcome Back! 🌱</h1>
       </div>
       <div className="flex flex-col items-center justify-center">
