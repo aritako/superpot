d50e91e33a09e433e15581383e94801d69b23a64
loridge <lagacho@up.edu.ph>
2024-05-24 23:11:13 +0800

    fix: modify login button

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -2,11 +2,10 @@ import { signOut } from "next-auth/react";
 
 export default function AuthButton({ hasLogin }: { hasLogin: boolean }) {
   const navBarFont =
-    "text-sm flex items-center rounded-full px-3 text-slate-900 hover:bg-green-200 hover:text-slate-900 transition duration-300 ease-in-out";
+    "h-[36px] text-sm flex items-center rounded-full px-3 text-white hover:bg-slate-800 transition duration-300 ease-in-out";
 
   const handleLogout = async () => {
     if (confirm("Are you sure you want to logout?")) {
-      sessionStorage.removeItem("uid");
       await signOut();
     }
   };
