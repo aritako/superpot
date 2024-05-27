b0e52af9087f222dff436ab6690c71765408fb97
Carmelo Atienza <atienzabusiness@gmail.com>
2024-05-27 22:29:44 +0800

    chore: omit comment

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from './firebase';
 import { getSession } from "next-auth/react";
-// This function can be marked `async` if using `await` inside
+
 export async function middleware(request) {
   const session = await getSession({ req: request });
   if(session){
