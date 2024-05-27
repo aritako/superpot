bb5abcef4ad8426bdca24bdd28f276abd9fe797c
Carmelo Atienza <atienzabusiness@gmail.com>
2024-05-27 16:56:49 +0800

    feat: add sidebar navs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,6 +7,7 @@ import { ReadData, SetData } from "../../components/RealtimeDatabase";
 import LightSensorChart from "@/components/LightSensorChart";
 import MoistureSensorChart from "@/components/MoistureSensorChart";
 import CurrentReadings from "@/components/CurrentReadings";
+import Sidebar from "@/components/Sidebar";
 // import { auth } from "../../firebase";
 // import { ref, get, onValue, set } from "firebase/database";
 // import { unsubscribe } from "diagnostics_channel";
@@ -82,41 +83,44 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <section className="flex-grow lg:p-12 sm:p-6 p-6">
-      <Navbar hasLogin={true} />
-      <div className="flex justify-center flex-col gap-10">
-        <div className="h-80 flex gap-4 justify-center">
-          <LightSensorChart data={lightData} />
-          <MoistureSensorChart data={moistData} />
-          <CurrentReadings
-            light={sensors.light}
-            moist={sensors.moist}
-            lidStat={lidStat}
-          />
-        </div>
+    <div className = "flex items-start justify-between">
+      <Sidebar />
+      <section className="flex-grow lg:p-12 sm:p-6 p-6 w-full h-full">
+        {/* <Navbar hasLogin={true} /> */}
+        <div className="flex justify-center flex-col gap-10">
+          <div className="h-80 flex gap-4 justify-center">
+            {/* <LightSensorChart data={lightData} />
+            <MoistureSensorChart data={moistData} />
+            <CurrentReadings
+              light={sensors.light}
+              moist={sensors.moist}
+              lidStat={lidStat}
+            /> */}
+          </div>
 
-        <div className="flex justify-center gap-4">
-          <button
-            className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-            onClick={() => {
-              fetchData();
-            }}
-          >
-            Refresh
-          </button>
-          <button
-            className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-            onClick={handleSubmit}
-          >
-            {manualLid ? "Close Lid" : "Open Lid"}
-          </button>
-          {/* Water plant button not yet implemented*/}
-          <button className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded">
-            Water Plant
-            {/* manualWater ? "Stop" : "Water Plant*/}
-          </button>
+          <div className="flex justify-center gap-4">
+            <button
+              className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+              onClick={() => {
+                fetchData();
+              }}
+            >
+              Refresh
+            </button>
+            <button
+              className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+              onClick={handleSubmit}
+            >
+              {manualLid ? "Close Lid" : "Open Lid"}
+            </button>
+            {/* Water plant button not yet implemented*/}
+            <button className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded">
+              Water Plant
+              {/* manualWater ? "Stop" : "Water Plant*/}
+            </button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,25 @@
 'use client';
+const navigation = [
+    { name: "Dashboard", href: "/dashboard"},
+    { name: "Logout", href: "/docs" },
+];
 export default function Sidebar(){
-    return (<div className = "w-[300px] border-r min-h-screen">
-    </div>)
+
+    return (
+    <div className = "w-[300px] min-w-[300px] border-r border-slate-800 min-h-screen flex flex-col">
+        <a href={"/"} className="flex items-center gap-1 p-3">
+        {/* <img src="/img/superpot-logo.png" alt="Logo" className="w-10 h-10" /> */}
+            <span className="krona_one text-sgreen">SuperPot</span>
+        </a>
+        {navigation.map((item) => (
+            <a
+            key={item.name}
+            href={item.href}
+            className="border border-x-0 border-slate-800 text-sm flex items-center px-4 py-3 text-white hover:bg-slate-800 transition duration-300 ease-in-out"
+            >
+            {item.name}
+            </a>
+        ))}
+    </div>
+    )
 }
