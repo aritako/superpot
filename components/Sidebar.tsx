14708beae788a3ca06a2c3c7b8f3531e0ebd02ee
Carmelo Atienza <atienzabusiness@gmail.com>
2024-05-30 09:36:22 +0800

    feat: render active path in dashboard

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,16 +1,17 @@
 'use client';
-
 import { signOut } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
 
 const navigation = [
     { name: "Dashboard", href: "/dashboard"},
-    { name: "Settings", href: "/dashboard" },
+    { name: "Settings", href: "" },
 ];
 
 const navbarItemStyle = "rounded-lg text-sm flex items-center px-4 py-3 text-white hover:bg-slate-800 transition duration-300 ease-in-out"
-
+const activeNavbarItemStyle = "bg-slate-800 rounded-lg text-sm flex items-center px-4 py-3 text-white";
 export default function Sidebar(){
+  const path = usePathname();
+  console.log(path)
   const handleLogout = () => {
     const doLogout = async () => {
       if (confirm("Are you sure you want to logout?")) {
@@ -21,7 +22,7 @@ export default function Sidebar(){
     doLogout();
   }
   return (
-  <div className = "sticky top-0 w-[250px] min-w-[250px] border-r border-slate-800 min-h-screen flex flex-col p-4 bg-slate-950">
+  <div className = "sticky top-0 w-[250px] min-w-[250px] border-r border-slate-800 min-h-screen flex flex-col p-4 bg-slate-950 gap-1">
     <a href={"/"} className="flex items-center gap-1 pb-4 ">
     {/* <img src="/img/superpot-logo.png" alt="Logo" className="w-10 h-10" /> */}
         <span className="krona_one text-sgreen text-xl">SuperPot</span>
@@ -30,7 +31,7 @@ export default function Sidebar(){
       <a
       key={item.name}
       href={item.href}
-      className= {navbarItemStyle}
+      className= {`${path == item.href ? activeNavbarItemStyle : navbarItemStyle} cursor-pointer`}
       >
       {item.name}
       </a>
