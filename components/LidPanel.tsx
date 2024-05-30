c69162b965ff0ace6d402171f58c30f9269b9048
Carmelo Atienza <atienzabusiness@gmail.com>
2024-05-30 23:43:03 +0800

    feat(lid): prioritize img loading

--- components/LidPanel.tsx
+++ components/LidPanel.tsx
@@ -10,9 +10,9 @@ const LidPanel : React.FC<LidPanelProps> = ({lidStat, light}) => {
   return (
     <div className="flex flex-col items-center">
       {light < 2000 || lidStat ? 
-      <Image src="/img/lidOpen.png" alt="Open" width={200} height={200} />
+      <Image priority = {true} src="/img/lidOpen.png" alt="Open" width={200} height={200} />
       : 
-      <Image src="/img/lidClose.png" alt="Close" width={200} height={200} />
+      <Image priority = {true} src="/img/lidClose.png" alt="Close" width={200} height={200} />
       }
       <span className = "krona_one text-2xl">{status}</span>
     </div>
