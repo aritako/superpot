8aa2627bde16b252526c9427f43daf8d6d4b3ece
Herminio Gregorio <hlgregorio@up.edu.ph>
2024-05-30 22:16:53 +0800

    feat: add clickable buttons pop up

--- components/ProductDesc.tsx
+++ components/ProductDesc.tsx
@@ -23,29 +23,130 @@ export default function ProductDesc() {
           style={{ width: "25%", height: "auto" }}
         />
       </div>
-      <Dialog>
-        <DialogTrigger
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            color: "white",
-          }}
-        >
-          CLICK ME
-        </DialogTrigger>
-
-        <DialogContent>
-          <img src="/img/soil_moisture_sensor.png" alt="product" />
-          <DialogHeader>
-            <DialogTitle>Soil Moisture Sensor</DialogTitle>
-            <DialogDescription>
-              Sensor which measures the current moisture of the soil for remote
-              monitoring.
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+      <div className="flex justify-center gap-5">
+        <SoilSensor />
+        <LightSensor />
+        <ServoMotor />
+        <WaterPump />
+        <ESP8266 />
+      </div>
     </main>
   );
 }
+
+const SoilSensor = () => (
+  <Dialog>
+    <DialogTrigger
+      style={{
+        color: "white",
+      }}
+    >
+      Soil Sensor
+    </DialogTrigger>
+
+    <DialogContent>
+      <img src="/img/soil_moisture_sensor.png" alt="product" />
+      <DialogHeader>
+        <DialogTitle>Soil Moisture Sensor</DialogTitle>
+        <DialogDescription>
+          Sensor which measures the current moisture of the soil for remote
+          monitoring.
+        </DialogDescription>
+      </DialogHeader>
+    </DialogContent>
+  </Dialog>
+);
+
+const LightSensor = () => (
+  <Dialog>
+    <DialogTrigger
+      style={{
+        color: "white",
+      }}
+    >
+      Light Sensor
+    </DialogTrigger>
+
+    <DialogContent>
+      <img src="/img/light_sensor.png" />
+      <DialogHeader>
+        <DialogTitle> Light Intensity Illumination Sensor</DialogTitle>
+        <DialogDescription>
+          Sensor which detects the intensity of incoming sunlight in front of
+          the greenhouse enclosure. Depending on the level of light intensity,
+          the motorized hinge mechanism will trigger to open or close.
+        </DialogDescription>
+      </DialogHeader>
+    </DialogContent>
+  </Dialog>
+);
+
+const ServoMotor = () => (
+  <Dialog>
+    <DialogTrigger
+      style={{
+        color: "white",
+      }}
+    >
+      Servo Motor
+    </DialogTrigger>
+
+    <DialogContent>
+      <img src="/img/servo_motor.png" />
+      <DialogHeader>
+        <DialogTitle>Servo Motor</DialogTitle>
+        <DialogDescription>
+          Actuator which motorizes the hinge mechanism of the device, enabling
+          the opening and closing of the greenhouse enclosure.
+        </DialogDescription>
+      </DialogHeader>
+    </DialogContent>
+  </Dialog>
+);
+
+const WaterPump = () => (
+  <Dialog>
+    <DialogTrigger
+      style={{
+        color: "white",
+      }}
+    >
+      Water Pump
+    </DialogTrigger>
+
+    <DialogContent>
+      <img src="/img/water_pump.png" />
+      <DialogHeader>
+        <DialogTitle>12V R385 Water Pump</DialogTitle>
+        <DialogDescription>
+          Actuator which enables water delivery from the reservoir to the
+          watering mechanism of the greenhouse enclosure
+        </DialogDescription>
+      </DialogHeader>
+    </DialogContent>
+  </Dialog>
+);
+
+const ESP8266 = () => (
+  <Dialog>
+    <DialogTrigger
+      style={{
+        color: "white",
+      }}
+    >
+      ESP8266
+    </DialogTrigger>
+
+    <DialogContent>
+      <img src="/img/esp8266.png" />
+      <DialogHeader>
+        <DialogTitle>WeMosD1 - ESP8266 Microcontroller</DialogTitle>
+        <DialogDescription>
+          The brain of the SuperPot that contains the ESP8266 Microcontroller
+          which provides processing power, memory, I/O capabilities, and
+          Integrated Wi-Fi.
+        </DialogDescription>
+      </DialogHeader>
+    </DialogContent>
+  </Dialog>
+);
