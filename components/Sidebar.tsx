1ea34351b0a447822ea9a527db482999154f3dae
Carmelo Atienza <atienzabusiness@gmail.com>
2024-05-31 20:05:53 +0800

    chore: omit comment

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -23,8 +23,7 @@ export default function Sidebar(){
   return (
   <div className = "sticky top-0 w-[250px] min-w-[250px] border-r border-slate-800 min-h-screen flex flex-col p-4 bg-slate-950 gap-1">
     <a href={"/"} className="flex items-center gap-1 pb-4 ">
-    {/* <img src="/img/superpot-logo.png" alt="Logo" className="w-10 h-10" /> */}
-        <span className="krona_one text-sgreen text-xl">SuperPot</span>
+      <span className="krona_one text-sgreen text-xl">SuperPot</span>
     </a>
     {navigation.map((item) => (
       <a
